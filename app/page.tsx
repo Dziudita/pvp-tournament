5918002fe049cc27b3d329febafcb7c855a1f531
Dziudita <Stakesheep.team@gmail.com>
2025-04-11 14:29:10 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,39 +4,35 @@ import Sidebar from "../components/Sidebar";
 import Header from "../components/Header";
 import MainContent from "../components/MainContent";
 import CherryChat from "../components/CherryChat";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex h-screen overflow-hidden">
+    <div className="flex h-screen w-full overflow-hidden">
       {/* Sidebar */}
       <Sidebar />
 
-      {/* Main Section */}
-      <div className="relative flex flex-col flex-grow bg-gradient-to-b from-black to-zinc-900">
-        {/* Header */}
+      {/* Main Content Area */}
+      <div className="flex flex-col flex-grow relative bg-gradient-to-b from-zinc-900 to-black">
         <Header />
-
-        {/* Main Content */}
         <MainContent />
-
-        {/* Bottom Cherries */}
-      <Image
-  src="/avatars/cool-cherry.png"
-  alt="Cool Cherry"
-  width={100}
-  height={100}
-  className="absolute bottom-4 left-4 z-40"
-/>
-<Image
-  src="/avatars/angry-cherry.png"
-  alt="Angry Cherry"
-  width={100}
-  height={100}
-  className="absolute bottom-4 right-4 z-40"
-/>
-
-        {/* Chat */}
         <CherryChat />
+
+        {/* Avatar Decorations */}
+        <Image
+          src="/avatars/cool-cherry.png"
+          alt="Cool Cherry"
+          width={120}
+          height={120}
+          className="absolute bottom-4 left-4 z-40 drop-shadow-[0_0_10px_#ff4dd6]"
+        />
+        <Image
+          src="/avatars/angry-cherry.png"
+          alt="Angry Cherry"
+          width={120}
+          height={120}
+          className="absolute bottom-4 right-4 z-40 drop-shadow-[0_0_10px_#ff4dd6]"
+        />
       </div>
     </div>
   );
