a0950639b71ac4569f7f4fecfd03decaffe45984
Dziudita <Stakesheep.team@gmail.com>
2025-04-11 10:45:37 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,28 @@
-// app/page.tsx
 "use client";
 
 import React from "react";
-import Header from "../components/Header";
 import Sidebar from "../components/Sidebar";
+import Header from "../components/Header";
 import MainContent from "../components/MainContent";
 import CherryChat from "../components/CherryChat";
 
 export default function Home() {
   return (
-    <div className="flex h-screen bg-gradient-to-b from-black to-zinc-900 text-white relative">
+    <div className="flex min-h-screen bg-gradient-to-b from-black to-zinc-900">
       {/* Sidebar */}
       <Sidebar />
 
-      {/* Main area */}
+      {/* Main Content Area */}
       <div className="flex flex-col flex-grow">
+        {/* Header */}
         <Header />
+
+        {/* Main */}
         <MainContent />
       </div>
 
-      {/* Chat */}
+      {/* Floating Cherry Chat */}
       <CherryChat />
-
-      {/* Cool Cherry (bottom-left) */}
-      <img
-        src="/avatars/cool-cherry.png"
-        alt="Cool Cherry"
-        className="absolute bottom-4 left-20 w-20 h-20 animate-bounce z-40"
-      />
-
-      {/* Angry Cherry (bottom-right) */}
-      <img
-        src="/avatars/angry-cherry.png"
-        alt="Angry Cherry"
-        className="absolute bottom-4 right-4 w-20 h-20 animate-pulse z-40"
-      />
     </div>
   );
 }
