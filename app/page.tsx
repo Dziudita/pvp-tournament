26c8d356ba5a54bb3851134c7f1c7a092b66c342
Dziudita <Stakesheep.team@gmail.com>
2025-04-11 10:36:12 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import CherryChat from "@/components/CherryChat";
 
 export default function Home() {
   return (
-    <div className="flex h-screen bg-gradient-to-b from-black to-zinc-900 text-white">
+    <div className="flex h-screen bg-gradient-to-b from-black to-zinc-900 text-white relative">
       {/* Sidebar */}
       <Sidebar />
 
@@ -19,21 +19,21 @@ export default function Home() {
         <MainContent />
       </div>
 
-      {/* Fixed Chat Icon */}
+      {/* Chat */}
       <CherryChat />
 
       {/* Cool Cherry (bottom-left) */}
       <img
         src="/avatars/cool-cherry.png"
         alt="Cool Cherry"
-        className="absolute bottom-4 left-20 w-20 h-20 animate-bounce"
+        className="absolute bottom-4 left-20 w-20 h-20 animate-bounce z-40"
       />
 
       {/* Angry Cherry (bottom-right) */}
       <img
         src="/avatars/angry-cherry.png"
         alt="Angry Cherry"
-        className="absolute bottom-4 right-4 w-20 h-20 animate-pulse"
+        className="absolute bottom-4 right-4 w-20 h-20 animate-pulse z-40"
       />
     </div>
   );
