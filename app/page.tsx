f9b88f7e55632e460794844724fdac081d786378
Dziudita <Stakesheep.team@gmail.com>
2025-04-11 14:06:44 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,39 @@
 "use client";
 
-import React from "react";
-import Header from "../components/Header";
-import Sidebar from "../components/Sidebar";
-import MainContent from "../components/MainContent";
-import CherryChat from "../components/CherryChat";
+import Sidebar from "@/components/Sidebar";
+import Header from "@/components/Header";
+import MainContent from "@/components/MainContent";
+import CherryChat from "@/components/CherryChat";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen bg-gradient-to-b from-black to-zinc-900">
+    <div className="flex h-screen overflow-hidden">
       {/* Sidebar */}
       <Sidebar />
 
-      {/* Main Content Area */}
-      <div className="flex flex-col flex-grow">
+      {/* Main Section */}
+      <div className="relative flex flex-col flex-grow bg-gradient-to-b from-black to-zinc-900">
         {/* Header */}
         <Header />
 
-        {/* Main */}
+        {/* Main Content */}
         <MainContent />
-      </div>
 
-      {/* Floating Cherry Chat */}
-      <CherryChat />
+        {/* Bottom Cherries */}
+        <img
+          src="/avatars/cool-cherry.png"
+          alt="Cool Cherry"
+          className="w-20 h-20 absolute bottom-4 left-4 z-10"
+        />
+        <img
+          src="/avatars/angry-cherry.png"
+          alt="Angry Cherry"
+          className="w-20 h-20 absolute bottom-4 right-4 z-10"
+        />
+
+        {/* Chat */}
+        <CherryChat />
+      </div>
     </div>
   );
 }
