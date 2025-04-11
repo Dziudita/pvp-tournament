a5e291bcfb0e5c015359756523b7f364dd32a1a0
Dziudita <Stakesheep.team@gmail.com>
2025-04-11 10:43:05 +0300

    Update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { FaGamepad, FaScroll, FaBrain, FaLifeRing, FaLock } from "react-icons/fa";
-import { MdLogout } from "react-icons/md";
 import Link from "next/link";
 import Image from "next/image";
 
@@ -11,22 +10,22 @@ const Sidebar = () => {
       <div>
         {/* Logo */}
         <div className="flex items-center gap-2 text-2xl font-bold text-pink-400 mb-10">
-          <Image src="/avatars/hammer-cherry.png" alt="logo" width={32} height={32} />
+          <Image src="/avatars/hammer-cherry.png" alt="Cherry Arena Logo" width={32} height={32} />
           CHERZI ARENA
         </div>
 
         {/* Navigation */}
         <nav className="flex flex-col gap-6 text-lg">
-          <Link href="#" className="flex items-center gap-2 hover:text-pink-400">
+          <Link href="/games" className="flex items-center gap-2 hover:text-pink-400">
             <FaGamepad /> Games
           </Link>
-          <Link href="#" className="flex items-center gap-2 hover:text-pink-400">
+          <Link href="/rules" className="flex items-center gap-2 hover:text-pink-400">
             <FaScroll /> Rules
           </Link>
-          <Link href="#" className="flex items-center gap-2 hover:text-pink-400">
+          <Link href="/about" className="flex items-center gap-2 hover:text-pink-400">
             <FaBrain /> About
           </Link>
-          <Link href="#" className="flex items-center gap-2 hover:text-pink-400">
+          <Link href="/support" className="flex items-center gap-2 hover:text-pink-400">
             <FaLifeRing /> Support
           </Link>
         </nav>
@@ -34,9 +33,9 @@ const Sidebar = () => {
 
       {/* Auth */}
       <div className="flex flex-col gap-4">
-        <button className="flex items-center gap-2 text-left hover:text-pink-400">
+        <Link href="/login" className="flex items-center gap-2 text-left hover:text-pink-400">
           <FaLock /> Login / Logout
-        </button>
+        </Link>
       </div>
     </aside>
   );
