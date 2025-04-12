57061bbc48b291212b9ad139c224d610de5bd7c6
Dziudita <Stakesheep.team@gmail.com>
2025-04-12 20:56:00 +0300

    Update walletConfig.ts

--- components/walletConfig.ts
+++ components/walletConfig.ts
@@ -1,30 +1,14 @@
-// components/walletConfig.tsx
-"use client";
+// components/walletConfig.ts
 
-import { EthereumClient, w3mConnectors, w3mProvider } from "@web3modal/ethereum";
-import { Web3Modal } from "@web3modal/react";
-import { configureChains, createConfig, WagmiConfig } from "wagmi";
-import { mainnet, polygon, arbitrum } from "wagmi/chains";
+import { getDefaultConfig } from "@rainbow-me/rainbowkit";
+import { mainnet, polygon, optimism, arbitrum, base, zora } from "wagmi/chains";
+import { createConfig } from "wagmi";
 
-const chains = [polygon]; // Galima keisti pagal poreikį
-
-const projectId = "YOUR_WEB3MODAL_PROJECT_ID"; // Gausi is https://cloud.walletconnect.com
-
-const { publicClient } = configureChains(chains, [w3mProvider({ projectId })]);
-
-const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors: w3mConnectors({ projectId, chains }),
-  publicClient,
-});
-
-const ethereumClient = new EthereumClient(wagmiConfig, chains);
-
-export default function WalletProvider({ children }: { children: React.ReactNode }) {
-  return (
-    <WagmiConfig config={wagmiConfig}>
-      {children}
-      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
-    </WagmiConfig>
-  );
-}
+export const wagmiConfig = createConfig(
+  getDefaultConfig({
+    appName: "Cherzi Arena",
+    projectId: "f08c4bbfc7b776ede46ee721df339102",
+    chains: [mainnet, polygon, optimism, arbitrum, base, zora],
+    ssr: true,
+  })
+);
