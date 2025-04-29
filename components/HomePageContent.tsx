ff57c6cb86dd756c44e1c288d5353aabf1e72ba8
Dziudita <Stakesheep.team@gmail.com>
2025-04-29 19:34:18 +0300

    Update HomePageContent.tsx

--- components/HomePageContent.tsx
+++ components/HomePageContent.tsx
@@ -2,9 +2,9 @@
 
 import { useState } from "react";
 import Image from "next/image";
-import Sidebar from "../components/Sidebar";
+import Sidebar from "@/components/Sidebar";
 import Topbar from "@/components/Topbar";
-import TopPlayerOfDay from "../components/TopPlayerOfDay";
+import TopPlayerOfDay from "@/components/TopPlayerOfDay";
 import TournamentRoomModal from "@/components/TournamentRoomModal";
 import TournamentSelectModal from "@/components/TournamentSelectModal";
 import CherryChat from "@/components/CherryChat";
@@ -20,7 +20,7 @@ export default function HomePageContent() {
 
   return (
     <div className="relative min-h-screen bg-black text-white overflow-hidden">
-      {/* Background */}
+      {/* Background image */}
       <Image
         src="/assets/vs-cherries-bg.png"
         alt="VS Cherries Background"
@@ -35,17 +35,17 @@ export default function HomePageContent() {
       {/* Fixed Topbar */}
       <Topbar collapsed={collapsed} />
 
-      {/* Scrollable main content */}
+      {/* Scrollable Main Content */}
       <div
         className={`
-          pt-16 pb-32 pr-4 pl-4
+          pt-20 pb-32 pr-4 pl-4
           transition-all duration-300
           ${collapsed ? 'ml-14' : 'ml-40'}
           h-screen overflow-y-auto relative z-10
         `}
       >
         {/* Tournament button */}
-        <div className="mt-8">
+        <div className="mt-8 flex justify-start">
           <Image
             src="/assets/tournament-button.png"
             alt="Tournament Button"
@@ -56,13 +56,13 @@ export default function HomePageContent() {
           />
         </div>
 
-        {/* Top player of the day */}
+        {/* Top Player of the Day */}
         <div className="mt-12">
           <TopPlayerOfDay />
         </div>
       </div>
 
-      {/* Modals */}
+      {/* Tournament Selection Modal */}
       {showTournamentSelectModal && (
         <TournamentSelectModal
           onSelect={(type: string) => {
@@ -74,6 +74,7 @@ export default function HomePageContent() {
         />
       )}
 
+      {/* Tournament Room Modal */}
       {showTournamentModal && selectedTournament && (
         <TournamentRoomModal
           tournamentType={selectedTournament}
@@ -81,7 +82,7 @@ export default function HomePageContent() {
         />
       )}
 
-      {/* Fixed Chat at bottom */}
+      {/* Fixed CherryChat at the bottom */}
       <div className="fixed bottom-0 left-0 right-0 z-50">
         <CherryChat />
       </div>
